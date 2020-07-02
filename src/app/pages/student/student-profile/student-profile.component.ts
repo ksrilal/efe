517ffe2c8ab89a7d6ddd97a224fc89193aeb0447
RajithaGayashan <rajithagayashan95@gmail.com>
2020-07-02 15:49:59 +0530

    Student profile updated

--- src/app/pages/student/student-profile/student-profile.component.ts
+++ src/app/pages/student/student-profile/student-profile.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-profile.component.scss']
 })
 export class StudentProfileComponent implements OnInit {
-
+  starRate = 2;
+  heartRate = 4;
+  radioGroupValue = 'This is value 2';
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  inputFile;
+  filename = 'Profile Photo';
+  uid;
+  photoURL = '../../assets/images/default-profile.jpg';
 
 }
