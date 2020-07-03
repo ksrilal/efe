846f505a549ee52c77e7d905c930c5596adec547
RajithaGayashan <rajithagayashan95@gmail.com>
2020-07-03 13:17:26 +0530

    form updated

--- src/app/pages/student/student-profile/student-profile.component.ts
+++ src/app/pages/student/student-profile/student-profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
+
 @Component({
   selector: 'ngx-student-profile',
   templateUrl: './student-profile.component.html',
@@ -14,9 +18,36 @@ export class StudentProfileComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // inputFile;
-  // filename = 'Profile Photo';
-  // uid;
-  // photoURL = '../../assets/images/default-profile.jpg';
+  inputFile;
+  filename = '';
+  uid;
+  photoURL = '../../assets/images/default-profile.jpg';
 
+}
+
+export class ImageUploadComponent {
+
+  selectedFile: ImageSnippet;
+
+  // constructor(private imageService: ImageService){}
+
+  processFile(imageInput: any) {
+    const file: File = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any) => {
+
+      this.selectedFile = new ImageSnippet(event.target.result, file);
+
+      // this.imageService.uploadImage(this.selectedFile.file).subscribe(
+      //   (res) => {
+        
+      //   },
+      //   (err) => {
+        
+      //   })
+    });
+
+    reader.readAsDataURL(file);
+  }
 }
