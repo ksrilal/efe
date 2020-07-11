fc02194fc3bffdfaf4c574668198cd96814c2d43
ksrilal <ksrilalranasinghe95@gmail.com>
2020-07-11 16:54:18 +0530

    navigation errors corect

--- src/app/pages/student-guard.service.ts
+++ src/app/pages/student-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from '../login/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StudentGuardService {
+export class StudentGuardService implements CanActivate{
+  constructor(private lg: LoginService, private router: Router) {}
 
-  constructor() { }
+  canActivate() {
+    var roleType = localStorage.getItem("role");
+    console.log("the role is " + roleType);
+    if (this.lg.isAuthenticated && roleType == "student") {
+      return true;
+    } else {
+      this.router.navigate(["/page"]);
+
+      return false;
+    }
+  }
 }
--- src/app/pages/teacher-guard.service.ts
+++ src/app/pages/teacher-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from '../login/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TeacherGuardService {
+export class TeacherGuardService implements CanActivate{
+  constructor(private lg: LoginService, private router: Router) {}
 
-  constructor() { }
+  canActivate() {
+    var roleType = localStorage.getItem("role");
+    console.log("the role is " + roleType);
+    if (this.lg.isAuthenticated && roleType == "teacher") {
+      return true;
+    } else {
+      this.router.navigate(["/page"]);
+
+      return false;
+    }
+  }
 }
