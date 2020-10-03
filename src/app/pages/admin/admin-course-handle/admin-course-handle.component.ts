364770cfb189ae90389a4cd466c6db8d2058c3e6
DilshanAbeywardhana <56632218+DilshanAbeywardhana@users.noreply.github.com>
2020-10-04 01:26:52 +0530

    modify course UI

--- src/app/pages/admin/admin-course-handle/admin-course-handle.component.ts
+++ src/app/pages/admin/admin-course-handle/admin-course-handle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'ngx-admin-course-handle',
@@ -10,6 +11,26 @@ export class AdminCourseHandleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  form = new FormGroup({
+    cuid: new FormControl("", Validators.required),
+  });
+
+  // form1 = new FormGroup({
+  //   name: new FormControl("", Validators.required),
+  //   tname: new FormControl("",Validators.required),
+  //   temail: new FormControl("",Validators.required),
+  //   date: new FormControl("",Validators.required),
+  //   time: new FormControl("",Validators.required),
+  // });
+
+  onSubmit() {
+    
+  }
+
+  get cuid() {
+    return this.form.get("cuid");
   }
 
 }
